1f4d26d24350b5ff25329209bf80e1d54aa8b3c8
lefeuvre Cel <celia.lefeuvre@laposte.net>
2022-07-12 12:35:45 +0200

    njl

--- ProjetFinal/src/main/java/com/intiFormation/entity/MyConstants.java
+++ ProjetFinal/src/main/java/com/intiFormation/entity/MyConstants.java
@@ -3,10 +3,10 @@ package com.intiFormation.entity;
 public class MyConstants {
 	
 
-    public static final String MY_EMAIL = "celia.lefeuvre@laposte.net";
+    public static final String MY_EMAIL = "celefru1609@gmail.com";
 
     
-    public static final String MY_PASSWORD = "!2LpbGoy8!";
+    public static final String MY_PASSWORD = "aelcpipplpxoxorb";
 	
 
 
