59cf74a161004881a83682b500da0e3266ff4d23
Sophiegerbel67 <gerbel.sophie67@gmail.com>
2022-07-13 09:15:28 +0200

    ngIf about

--- ProjetFinal/src/main/java/com/intiFormation/entity/MyConstants.java
+++ ProjetFinal/src/main/java/com/intiFormation/entity/MyConstants.java
@@ -5,13 +5,8 @@ public class MyConstants {
     public static final String MY_EMAIL = "gerbel.sophie67@gmail.com";
 
     
-<<<<<<< HEAD
-    public static final String MY_PASSWORD = "ytjwvgpjqssqakze";
-=======
-    public static final String MY_PASSWORD = "aelcpipplpxoxorb";
 
->>>>>>> 8e829629e3794ebe871fbb113f2681368d0cc2b0
-	
+    public static final String MY_PASSWORD = "ytjwvgpjqssqakze";
 
 
 }
